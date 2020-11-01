75ae98222ab7e7207443e121cc40759cd209d196
Justas Mundeikis <mundeikis@gmx.de>
2020-11-01 19:08:34 +0200

    automatic update 2020-11-01 19:08

--- push.bash
+++ push.bash
@@ -6,12 +6,16 @@
 # Date_modification
 
 date=$(date +"%Y-%m-%d %H:%M");
-echo "started $(date)">> /home/pc/Data/Projects/corona-stat-lt-blog/log.txt;
+echo "started build $(date)">> /home/pc/Data/Projects/corona-stat-lt-blog/log.txt;
 
 #git pull;
-Rscript -e "blogdown::build_dir(dir='/home/pc/Data/Projects/corona-stat-lt-blog/', force=TRUE)";
+Rscript -e "blogdown::build_dir(dir='/home/pc/Data/Projects/corona-stat-lt-blog', force=TRUE)";
+
+date=$(date +"%Y-%m-%d %H:%M");
+echo "finished build $(date)" >> /home/pc/Data/Projects/corona-stat-lt-blog/log.txt;
+
 git add . ;
 git commit -m "automatic update $date";
 git push;
 
-echo "finished $(date)" >> /home/pc/Data/Projects/corona-stat-lt-blog/log.txt
+
