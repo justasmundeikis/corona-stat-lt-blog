7f3a315620e27a1a76c94a137839d7c2f1ec86b6
Justas Mundeikis <mundeikis@gmx.de>
2020-11-01 18:55:10 +0200

    automatic update 2020-11-01 18:55

--- push.bash
+++ push.bash
@@ -9,9 +9,6 @@ date=$(date +"%Y-%m-%d %H:%M");
 echo "started $(date)">> ./log.txt;
 
 #git pull;
-Rscript -e "blogdown::build_dir()";
-git add .;
-git commit -m "automatic update $date";
-git push;
+Rscript -e "blogdown::build_dir()"&& git add . && git commit -m "automatic update $date" && git push;
 
 echo "finished $(date)">> ./log.txt
